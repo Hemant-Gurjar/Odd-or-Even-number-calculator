a9bc05999f197a55fa2eadcb73a6429bf7c61191
Hemant <73576412+Hemant-Gurjar@users.noreply.github.com>
2022-01-11 05:10:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+number = int(input("Which number do you want to check? "))
+
+mod_number = number % 2 
+
+if mod_number == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")    
+
